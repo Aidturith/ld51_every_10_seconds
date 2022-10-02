d5828505242ac8d79bbe87938d3562c0e1d356fd
Aidturith <aidturith147@gmail.com>
2022-10-02 11:19:23 +0200

    Rename scenes to make more sense

--- src/scripts/Game.gd
+++ src/scripts/Game.gd
@@ -4,8 +4,6 @@ var images = []
 var picks = []
 var selected: ImagePrompt = null
 
-var choose_scn = load("res://scenes/ChooseImage.tscn")
-
 func _ready():
 	print("load game")
 	randomize()
@@ -13,10 +11,7 @@ func _ready():
 	var image_a = pick_image()
 	var image_b = pick_image()
 	self.picks = [image_a, image_b]
-	$Scenes/ChooseImage.init(picks)
-	#var choose_node = choose_scn.instance()
-	#choose_node.init(picks)
-	#self.add_child(choose_node)
+	$Scenes/Choosing.init(picks)
 
 func load_images(path: String):
 	var scene = load("res://scenes/ImagePrompt.tscn")
@@ -40,16 +35,16 @@ func pick_image():
 		var pick = randi() % self.images.size()
 		return self.images.pop_at(pick)
 
-func _on_ChooseImage_image_selected(index):
+func _on_Choosing_image_selected(index):
 	self.selected = picks[index]
-	$Scenes/ChooseImage.hide()
-	$Scenes/ChooseImage.pause_mode = Node.PAUSE_MODE_STOP
-	$Scenes/TitleImage.init(selected)
+	$Scenes/Choosing.hide()
+	$Scenes/Choosing.pause_mode = Node.PAUSE_MODE_STOP
+	$Scenes/Typing.init(selected)
 
 func _on_TickingClock_clock_stop():
-	$Scenes/TitleImage/LineEdit.editable = false
-	$Scenes/TitleImage.hide()
-	$Scenes/TitleImage.pause_mode = Node.PAUSE_MODE_STOP
+	$Scenes/Typing/LineEdit.editable = false
+	$Scenes/Typing.hide()
+	$Scenes/Typing.pause_mode = Node.PAUSE_MODE_STOP
 	var scores = get_scores()
 	$Scenes/Result.init(selected)
 	$Scenes/Result.update_score(scores)
@@ -57,7 +52,7 @@ func _on_TickingClock_clock_stop():
 	$Scenes/Result.pause_mode = Node.PAUSE_MODE_INHERIT
 
 func get_scores():
-	var title_tokens = $Scenes/TitleImage/LineEdit.text.split(" ", false)
+	var title_tokens = $Scenes/Typing/LineEdit.text.split(" ", false)
 	var scores = []
 	for a in title_tokens:
 		var score_max = 0.0
